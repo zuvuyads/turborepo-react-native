46701a76e954089d96c8c2e582d1b208193841f8
Sean Dinwiddie <seandinwiddie@gmail.com>
2025-04-14 16:59:14 -0700

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,8 +7,8 @@ import styles from "../styles/index.module.css";
 export default function Web() {
   return (
     <div className={styles.container}>
-      <h1>Web</h1>
-      <Button onClick={() => console.log("Pressed!")} text="Boop" />
+      <h1>ZuvuyaDS W</h1>
+      <Button onClick={() => console.log("Pressed!")} text="In progress" />
     </div>
   );
 }
